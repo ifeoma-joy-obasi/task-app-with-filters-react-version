21f6bfab8ae10ae49230347b0321300da1cd54b0
ifeoma-joy-obasi <ifeomaobasi1999@gmail.com>
2023-11-25 14:12:56 +0100

    first commit

---
+++ src/Components/FilterByStatus.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import filterIcon from "../Images/filter-76.svg";
+
+const FilterByStatus = () => {
+  const [activeButton, setActiveButton] = useState(null);
+  
+  const btns = ['All', 'Done', 'In Progress', 'Todo'];
+  
+    return (
+      <section>
+        <h3 className="filter-header">Filter by status</h3>
+        <div className="filter-by-status-wrapper">
+          <div className="btn-container">
+            {btns.map((btn, index) => (
+              <button
+                key={index + 1}
+                style={{
+                  backgroundColor:
+                    activeButton === index ? "#dbf9f9" : "#f3ebeb",
+                  color: activeButton === index ? "#319797" : " #221f1f",
+                }}
+                onClick={() => setActiveButton(index)}
+                className="status-btn"
+              >
+                {btn}
+              </button>
+            ))}
+          </div>
+
+          <div className="icon">
+            <img src={filterIcon} alt="filter icon" className="filter-icon" />
+          </div>
+        </div>
+      </section>
+    );
+}
+
+export default FilterByStatus
